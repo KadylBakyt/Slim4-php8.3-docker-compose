757028dd9178b79a055317a144a8f0d3f8140e49
Kadyl Bakyt <k.bakyt@recycle.kz>
2024-05-06 18:11:22 +0500

    first commit

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Selective\BasePath\BasePathMiddleware;
+use Slim\Factory\AppFactory;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->addRoutingMiddleware();
+$app->add(new BasePathMiddleware($app));
+$app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+   $response->getBody()->write('Hello World!');
+   return $response;
+});
+
+$app->run();
